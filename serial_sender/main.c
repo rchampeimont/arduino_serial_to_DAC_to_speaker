7cc549402cac025d07f647c35e81b325aaecf523
Raphaël Champeimont <raphael.champeimont@almacha.org>
2021-05-06 10:00:06 +0200

    add comments in C code

--- serial_sender/main.c
+++ serial_sender/main.c
@@ -6,11 +6,14 @@
 int main(int argc, char *argv[]) {
 	int readByte;
 	
+	// Replace COM4 with your own virtual serial port connected to the Arduino
 	system("mode com4 baud=450000 data=8 parity=n to=off");
 	
+	// The file must be in Microsoft WAV format in Unsigned 8-bit mono.
 	//FILE *f = fopen("../sweet_dreams_8bit_mono.wav", "rb");
 	FILE *f = fopen("../take_on_me_unsigned_8bit_mono.wav", "rb");
 	
+	// Replace COM4 with your own virtual serial port connected to the Arduino
 	FILE *serialPort = fopen("COM4", "wb");
 	
 	printf("Sending data to COM port...\n");
