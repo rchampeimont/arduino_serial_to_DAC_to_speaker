51fcae262a4a26af57ade7ed4ee1aad691a633b0
Raphaël Champeimont <raphael.champeimont@almacha.org>
2021-05-18 19:29:03 +0200

    Use a constant instead of hardcoding COM4

--- serial_sender/main.c
+++ serial_sender/main.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define PORT "COM4"
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	int readByte;
 	
 	// Replace COM4 with your own virtual serial port connected to the Arduino
-	system("mode com4 baud=450000 data=8 parity=n to=off");
+	system("mode " PORT " baud=450000 data=8 parity=n to=off");
 	
 	// The file must be in Microsoft WAV format in Unsigned 8-bit mono.
 	//FILE *f = fopen("../sweet_dreams_8bit_mono.wav", "rb");
 	FILE *f = fopen("../take_on_me_unsigned_8bit_mono.wav", "rb");
 	
 	// Replace COM4 with your own virtual serial port connected to the Arduino
-	FILE *serialPort = fopen("COM4", "wb");
+	FILE *serialPort = fopen(PORT, "wb");
 	
-	printf("Sending data to COM port...\n");
+	printf("Sending data to " PORT " port...\n");
 	
-	while ((readByte = getc(f)) != EOF) {
-		putc(readByte, serialPort);
+	while (1) {
+		while ((readByte = getc(f)) != EOF) {
+			putc(readByte, serialPort);
+		}
+		rewind(f);
 	}
+	
 	return 0;
 }
