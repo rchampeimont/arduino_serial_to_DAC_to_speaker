919501474173adee6b1b4034548b27cdd6b794b8
Raphaël Champeimont <raphael.champeimont@almacha.org>
2021-05-19 10:08:50 +0200

    optimize write speed

--- serial_to_DAC_to_speaker/serial_to_DAC_to_speaker.ino
+++ serial_to_DAC_to_speaker/serial_to_DAC_to_speaker.ino
@@ -15,8 +15,15 @@ void loop() {
   int value = Serial.read();
   if (value >= 0) {
     byte valueByte = value;
-    PORTB = valueByte >> 2; // sets pins 8 to 13
-    digitalWrite(7, (valueByte >> 1) & 0x1);
-    digitalWrite(6, valueByte & 0x1);
+    PORTB = valueByte >> 2; // sets pins 8 to 13 to the 6 most significant bits
+
+    // with digitalWrite, least significant bit would change 7 to 8 microseconds after PORTB
+    //digitalWrite(7, (valueByte >> 1) & 0x1);
+    //digitalWrite(6, valueByte & 0x1);
+
+    // with this optimized version, least significant bit changes 0.4 to 0.7 microsecond after PORTB
+    byte shiftedByte = valueByte << 6;
+    PORTD |= shiftedByte & B11000000; // set pins 6 and 7 to 1 if needed
+    PORTD &= shiftedByte | B00111111; // set pins 6 and 7 to 0 if needed
   }
 }
